9787358bfff31025adfd4c7860f3505eb4303e7c
EmiRoberti77 <emi@datamotion.ai>
2024-08-29 06:03:39 +0100

    updated files

--- app/websocket/page.tsx
+++ app/websocket/page.tsx
@@ -1,42 +1,54 @@
 "use client";
 import { useEffect, useState } from "react";
 const socket_url = process.env.NEXT_PUBLIC_SOCKET_URL!;
+interface WSSMessages {
+  message: any;
+}
 
 export default function WebSocketPage() {
   const [isConnected, setIsConnected] = useState(false);
   const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [messages, setMessages] = useState<WSSMessages[]>([]);
   useEffect(() => {
     console.log(socket_url);
     const ws = new WebSocket(socket_url);
-    ws.onopen = () => {
-      console.log("socket opened");
-      setIsConnected(true);
-    };
-    // ws.onclose = () => {
-    //   console.log("connection closed");
-    //   setIsConnected(false);
-    // };
-    ws.onmessage = (message) => {
-      console.log(message.data);
-    };
-    ws.onerror = (error) => {
-      console.log(error);
-    };
+    ws.addEventListener("open", (e) => {
+      console.log(e, "connection open");
+    });
+
+    ws.addEventListener("message", (e) => {
+      console.log(e.data);
+      const msg: WSSMessages = {
+        message: e.data,
+      };
+      setMessages((prev) => [...prev, msg]);
+    });
+
     setSocket(ws);
     return () => {
+      console.log("closing socket");
       ws.close();
     };
   }, []);
+
   const sendMessage = () => {
     const message = {
       action: "sendMessage",
-      connectionId: "",
       message: "hello nextjs",
     };
+
+    if (!socket) {
+      console.log("socket null");
+    }
+    console.log(message);
+    socket?.send(JSON.stringify(message));
   };
   return (
     <div>
-      <button></button>
+      <button onClick={sendMessage}>SEND</button>
+      {messages.map((msg, index) => (
+        <li key={index}>{msg.message}</li>
+      ))}
     </div>
   );
 }
