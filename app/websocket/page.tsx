ffc59529a6af57f806d4d54e442c6e3bec2eb274
EmiRoberti77 <emi@datamotion.ai>
2024-09-17 07:33:24 +0100

    modified code to support new registration

--- app/websocket/page.tsx
+++ app/websocket/page.tsx
@@ -111,6 +111,7 @@ export default function WebSocketPage() {
       id,
       site,
       connectionId,
+      lastActiveStateDateTime: new Date().toISOString(),
     };
 
     const response = await fetch(registerEndPoint, {
